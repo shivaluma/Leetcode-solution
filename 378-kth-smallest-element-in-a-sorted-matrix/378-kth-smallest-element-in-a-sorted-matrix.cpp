2f765661b49481fbcb83abb886d00a3f79f42d16
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2022-08-02 14:42:10 +0700

    Time: 48 ms (63.09%), Space: 13 MB (99.39%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>> &matrix, int k) {
-        int m = matrix.size(), n = matrix[0].size(), ans;
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> minHeap;
-        for (int r = 0; r < min(m, k); ++r)
-            minHeap.push({matrix[r][0], r, 0});
+    int kthSmallest(vector<vector<int>>& matrix, int k) 
+    {
+        int N = matrix.size();
+        int left = matrix[0][0];
+        int right = matrix[N-1][N-1];
+        while (left<right)
+        {
+            int mid = left+(right-left)/2;
 
-        for (int i = 1; i <= k; ++i) {
-            auto top = minHeap.top(); minHeap.pop();
-            int r = top[1], c = top[2];
-            ans = top[0];
-            if (c + 1 < n) minHeap.push({matrix[r][c + 1], r, c + 1});
+            if (equalOrSmaller(matrix,mid)<k)
+                left = mid+1;
+            else
+                right = mid;
         }
-        return ans;
+        return left;
+    }
+    
+    int equalOrSmaller(vector<vector<int>>& matrix, int x)
+    {
+        int N = matrix.size();
+        int i=N-1, j=0, count = 0;
+        while (i>=0 && j<N)
+        {
+            if (matrix[i][j]<=x)
+            {
+                count+=(i+1);
+                j++;
+            }
+            else
+                i--;                
+        }
+        return count;
     }
 };
