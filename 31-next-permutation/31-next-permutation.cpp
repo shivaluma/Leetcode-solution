5308f2ff340b14c64f45ad7afa3cb5a9db80520f
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2022-08-28 02:10:49 +0700

    Time: 13 ms (13.71%), Space: 12.1 MB (74.79%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& permutation) {
+    int n = permutation.size(), k,l;
+    for(k=n-2; k>=0; k--){
+        if(permutation[k]<permutation[k+1]) break;
+    }
+    if(k<0){
+        reverse(permutation.begin(),permutation.end());
+    }else{
+        for(l=n-1; l>k; l--){
+            if(permutation[k]<permutation[l]) break;
+        }
+        swap(permutation[k],permutation[l]);
+        reverse(permutation.begin()+k+1, permutation.end());
+    }
+}
+};
