eaad2a78c6d2a6181bfe9485091cc73962355b32
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2022-09-04 23:18:14 +0700

    Time: 1581 ms (5.00%), Space: 13 MB (95.66%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,31 +11,29 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* res = nullptr;
-        ListNode* curr = nullptr;
-        while(lists.size()){
-            int idx = 0;
-            if(!lists[idx]) {
-                lists.erase(lists.begin() + idx);
-                continue;
-            }
-            for(int i = 1; i < lists.size(); i++){
-                if(lists[i] && lists[idx]->val > lists[i]->val) {
-                    idx = i;
+        ListNode* headAns = new ListNode(0);
+        ListNode* tailAns = headAns;
+
+        while (lists.size()) {
+            int minIdx = 0;
+            for (int i = 0; i < lists.size(); i++) {  
+                
+                if (!lists[i]) {
+                    lists.erase(lists.begin() + i);
+                }
+                
+                if (lists[i] && lists[i]->val < lists[minIdx]->val) {
+                    minIdx = i;
                 }
             }
-                        
-            if(!curr) curr = lists[idx];
-            else { 
-                curr->next = lists[idx];
-                curr = curr->next;
-            }
-            if(!res) res = curr;
-
-            if(lists[idx]) lists[idx] = lists[idx]->next;
-            if(lists[idx] == nullptr) lists.erase(lists.begin() + idx);
+            
+           
+            
+            tailAns->next = lists[minIdx];
+            if (lists[minIdx]) lists[minIdx] = lists[minIdx]->next;
+            if (tailAns->next) tailAns = tailAns->next;
         }
         
-        return res;
+        return headAns->next;
     }
 };
