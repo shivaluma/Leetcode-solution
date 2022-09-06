b76f69d425d4f1b833c4c40d1b1bfb3ec30bb54c
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2022-09-06 21:13:23 +0700

    Time: 3 ms (98.74%), Space: 6.1 MB (97.48%) - LeetHub

--- 854-k-similar-strings/854-k-similar-strings.cpp
+++ 854-k-similar-strings/854-k-similar-strings.cpp
@@ -1,34 +1,92 @@
+// class Solution {
+// public:
+//     int ans = INT_MAX;
+//     int kSimilarity(string s1, string s2) {
+        
+//         solve(s1,s2,0,0);
+        
+//         return ans;
+//     }
+    
+//     void solve(string &s1, string &s2, int i, int step) {
+        
+//         if (step > ans) return;
+        
+//         if (i == s1.length()) {
+//             ans = min(ans, step);
+//             return;
+//         }
+        
+//         if (s1[i] == s2[i]) {
+//             solve(s1,s2,i+1, step);
+//             return;
+//         }
+        
+        
+//         for (int j = i+1;j < s1.length(); j++) {
+//             if (s1[i] == s2[j]) {
+//                 swap(s2[i], s2[j]);
+//                 solve(s1,s2,i+1,step+1);
+//                 swap(s2[i], s2[j]);
+//             }
+//         }
+//     }
+// };
+
+
+
 class Solution {
 public:
     int ans = INT_MAX;
-    int kSimilarity(string s1, string s2) {
-        
-        solve(s1,s2,0,0);
-        
-        return ans;
-    }
-    
-    void solve(string &s1, string &s2, int i, int step) {
-        
-        if (step > ans) return;
-        
-        if (i == s1.length()) {
-            ans = min(ans, step);
+    void dfs(string &s1 , string &s2 , int i , int count)
+    {
+        //cout << s1 << ',' << s2 << ',' << i << ',' << count << '\n';
+       
+        if (s1 == s2)
+        {
+            ans = min(count , ans);
             return;
         }
+            
         
-        if (s1[i] == s2[i]) {
-            solve(s1,s2,i+1, step);
-            return;
+        if (s1[i] == s2[i])
+        {
+           dfs(s1 , s2 , i + 1 , count );
         }
-        
-        
-        for (int j = i+1;j < s1.length(); j++) {
-            if (s1[i] == s2[j]) {
-                swap(s2[i], s2[j]);
-                solve(s1,s2,i+1,step+1);
-                swap(s2[i], s2[j]);
+        else        
+        {
+            for (int j = i + 1 ; j < s1.size() ; j ++)
+            {
+                if (s1[j] == s2[i] && s1[i] != s2[i] && s1[j] != s2[j] && s1[i] == s2[j])
+                {
+                    swap(s1[i] , s1[j]);
+                    dfs(s1 , s2 , i + 1 , count + 1 );
+                    swap(s1[i] , s1[j]);
+                    return;
+                }
+            }
+            
+            for (int j = i + 1 ; j < s1.size() ; j ++)
+            {
+                if (s1[j] == s2[i] && s1[i] != s2[i] && s1[j] != s2[j])
+                {
+                    swap(s1[i] , s1[j]);
+                    dfs(s1 , s2 , i + 1 , count + 1 );
+                    swap(s1[i] , s1[j]);
+                }
             }
         }
+        
+        
+            
+    }
+    
+    
+    
+    int kSimilarity(string s1, string s2) {
+        vector<int> min_count(22 , INT_MAX);
+        unordered_set<string> k;
+        dfs(s1 , s2 , 0 , 0);
+        return ans;
     }
 };
