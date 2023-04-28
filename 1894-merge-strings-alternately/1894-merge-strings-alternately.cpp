e11d9dca1b7565e127b9ee66d49708310b2aef1d
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2023-04-28 19:32:46 +0700

    Time 4 ms (41%) - Space 6.4 MB (59%) - Leetcrawl

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mergeAlternately(string s1, string s2) {
+        string result = "";
+ 
+    
+    for (int i = 0; i < s1.length() ||
+                    i < s2.length(); i++) 
+    { 
+ 
+       
+        if (i < s1.length()) 
+            result += s1[i];
+ 
+       
+        if (i < s2.length()) 
+            result += s2[i];
+    } 
+    return result; 
+    }
+};
