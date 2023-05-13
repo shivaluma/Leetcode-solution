7ecebf25f50e13a1587d8b153933a29e48f1303e
Nguyen Viet Thanh <33391636+shivaluma@users.noreply.github.com>
2023-05-13 15:59:03 +0700

    Time: 22 ms (31%) - Space: 9.3 MB (82%) - Leetcrawl

---
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int>  dp(high+1, 0);
+        dp[0] = 1;
+        int mod = 1000000007;
+        for (int end = 1; end <= high; end++) {
+            if (end >= zero) {
+                dp[end] += dp[end-zero];
+            }
+
+            if (end >= one) {
+                dp[end] += dp[end-one];
+            }
+
+            dp[end] %= mod;
+        }
+
+        int ans = 0;
+
+        for (int i = low; i <= high; i++) {
+            ans += dp[i];
+            ans %= mod;
+        }
+
+        return ans;
+    }
+};
